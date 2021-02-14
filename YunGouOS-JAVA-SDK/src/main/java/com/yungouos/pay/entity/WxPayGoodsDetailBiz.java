40c29c6d79c3b48bc42b425fcbaa13aa873d5485
YunGouOS <server@yungouos.com>
2021-02-14 14:46:09 +0800

    新增退款到账异步回调
    1、新增退款到账异步回调
    2、微信对账单下载接口新增结束日期，可下载某区间内的对账单
    3、升级hutool版本至5.5.8

---
+++ YunGouOS-JAVA-SDK/src/main/java/com/yungouos/pay/entity/WxPayGoodsDetailBiz.java
@@ -0,0 +1,87 @@
+package com.yungouos.pay.entity;
+
+import java.io.Serializable;
+
+/**
+ * 
+ * 微信商品详情
+ * 
+ * 
+ * @action
+ *
+ * @author YunGouOS技术部-029
+ *
+ * @time 2021年2月6日 上午10:43:46
+ *
+ *
+ */
+public class WxPayGoodsDetailBiz implements Serializable {
+
+	private static final long serialVersionUID = -8252785620981441230L;
+
+	/**
+	 * 商品编码
+	 */
+	private String goods_id;
+
+	/**
+	 * 微信侧商品编码
+	 */
+	private String wxpay_goods_id;
+
+	/**
+	 * 商品的实际名称
+	 */
+	private String goods_name;
+
+	/**
+	 * 用户购买的数量
+	 */
+	private Integer quantity;
+
+	/**
+	 * 商品单价
+	 */
+	private Integer price;
+
+	public String getGoods_id() {
+		return goods_id;
+	}
+
+	public void setGoods_id(String goods_id) {
+		this.goods_id = goods_id;
+	}
+
+	public String getWxpay_goods_id() {
+		return wxpay_goods_id;
+	}
+
+	public void setWxpay_goods_id(String wxpay_goods_id) {
+		this.wxpay_goods_id = wxpay_goods_id;
+	}
+
+	public String getGoods_name() {
+		return goods_name;
+	}
+
+	public void setGoods_name(String goods_name) {
+		this.goods_name = goods_name;
+	}
+
+	public Integer getQuantity() {
+		return quantity;
+	}
+
+	public void setQuantity(Integer quantity) {
+		this.quantity = quantity;
+	}
+
+	public Integer getPrice() {
+		return price;
+	}
+
+	public void setPrice(Integer price) {
+		this.price = price;
+	}
+
+}
