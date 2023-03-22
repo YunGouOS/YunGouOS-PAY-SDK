f3bffac9003e2b37560a1151d44443b9fa62fc59
YunGouOS <server@yungouos.com>
2023-03-22 20:03:11 +0800

    更新配置
    更新配置

--- YunGouOS-WxApp-SDK/wxpay/config.js
+++ YunGouOS-WxApp-SDK/wxpay/config.js
@@ -1,6 +1,6 @@
-let mch_id = '1529637931'//微信支付商户号，登录YunGouOS.com 微信支付-》商户管理 查看
+let mch_id = ''//微信支付商户号，登录YunGouOS.com 微信支付-》商户管理 查看
 
-let key = '499F61DB734C4BF39792A098C44FA80A';//支付密钥 登录YunGouOS.com 微信支付-》商户管理 查看
+let key = '';//支付密钥 登录YunGouOS.com 微信支付-》商户管理 查看
 
 
 /**
